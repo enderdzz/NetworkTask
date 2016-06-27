7548d0fef5c9292e1885cced93cd79ef0a44c88a
enderdzz <417621265@qq.com>
2016-06-28 02:05:53 +0800

    fix the socket bugs and Mod number

--- Go_Back_N/receiver.c
+++ Go_Back_N/receiver.c
@@ -13,6 +13,7 @@
 #define WindowSize 15
 #define P1 50
 #define P2 5
+#define Mod 16
 
 char act[10];
 char bad[20] = "Time-out ";
@@ -24,61 +25,63 @@ unsigned int sseed;
 
 int main()
 {
-    s = socket(AF_INET, SOCK_STREAM, 0); /* use TCP protocol */
-  	if(s == -1){
-  		puts("Building a socket failed!");
-  		return 0;
-  	}
+	s = socket(AF_INET, SOCK_STREAM, 0); /* use TCP protocol */
+	if(s == -1){
+		puts("Building a socket failed!");
+		return 0;
+	}
 
-    ser.sin_family = AF_INET;
-    ser.sin_port = htons(6500);
-    ser.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ser.sin_family = AF_INET;
+	ser.sin_port = htons(6500);
+	ser.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    err = bind(s, (struct sockaddr *) &ser, sizeof(ser));
-  	if(err != 0){
-  		puts("Binding a socket failed!");
-  		return 0;
-  	}
+	err = bind(s, (struct sockaddr *) &ser, sizeof(ser));
+	if(err != 0){
+		puts("Binding a socket failed!");
+		return 0;
+	}
 
-    listen(s, 1);
-    int len = sizeof(cli);
-    sock = accept(s, (struct sockaddr *) &cli, &len);
-    puts("TCP Connection Established.");
+	listen(s, 1);
+	int len = sizeof(cli);
+	sock = accept(s, (struct sockaddr *) &cli, &len);
+	puts("TCP Connection Established.");
 
-    sseed=(unsigned int) time(NULL);
-    srand(sseed);
-    recv(sock, act, sizeof(act), 0);
-    total = atoi(act);
+	sseed=(unsigned int) time(NULL);
+	srand(sseed);
+	recv(sock, act, sizeof(act), 0);
+	total = atoi(act);
 
-/************************* go back n **************************/
-    int i, cur = 0;
-    while(1) {
+	/************************* go back n **************************/
+	int i, cur = 0;
+	while(1) {
 
-        recv(sock,act,sizeof(act),0);
-        sleep(1);
-        if(strcmp(act, bad) == 0){ // if sender send bad, then receiver do nothing.
+		//recv(sock,act,sizeof(act), MSG_DONTWAIT);
+		recv(sock,act,sizeof(act), 0);
+		sleep(1);
+		if(strcmp(act, bad) == 0){ // if sender send bad, then receiver do nothing.
 
-          continue;
-        }
+			continue;
+		}
 
-        int check = atoi(act);
-        int random = rand() % P1;
-        if(random < P2 && check == cur && cur < total){
+		int check = atoi(act);
+		int random = rand() % P1;
+		// printf("check = %d, random = %d\n", check, random);
+		if(random < P2 && check == cur && cur < total){
 
-          printf("Frame %s Received \n", act);
-          cur++;
-        }
-        if(check == cur && cur < total){
-          send(sock, act, sizeof(act), 0);
-          printf("Frame %s Received \n", act);
-          cur++;
-        }
-        if(cur >= total) break;
+			printf("Frame %d Received \n", check % Mod);
+			cur++;
+		}
+		if(check == cur && cur < total){
+			send(sock, act, sizeof(act), 0);
+			printf("Frame %d Received \n", check % Mod);
+			cur++;
+		}
+		if(cur >= total) break;
 
-    }
-    close(sock);
-    close(s);
-    return 0;
+	}
+	close(sock);
+	close(s);
+	return 0;
 }
 
 void int2char(int z)
--- Go_Back_N/sender.c
+++ Go_Back_N/sender.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <signal.h>
 
 #include <sys/types.h>
@@ -15,137 +14,147 @@
 #define WindowSize 3
 #define TimerNum 50
 #define P1 50
-#define P2 5
+#define P2 10
+#define Mod 16
 
-char act[10];
-char bad[20] = "Time-out ";
-int s, total;
-int begin, end, debug, cur;
-void int2char(int);
-void timer_on(int);
-void setTimer(int, int);
-void timeout();
-void process();
-void reset_process(int);
-int judge(int, int);
+char act[10];               // 伪帧
+char bad[20] = "Time-out "; // 伪超时标志
+int s, total;								// total 帧总数 s socket描述符
+int begin, end, debug, cur; // begin与end均为现存计时器的标号 debug为窗口内第一个超时的计时器标号 cur为待发送帧指针
+void int2char(int);			 /*发送的文本字符串*/
+void timer_on(int);      /*打开计时器(标号)*/
+void setTimer(int, int); /*设定计时器(时限, 标号)*/
+void timeout();          /*计时器的唯一操作*/
+void process();          /*运行计时器*/
+void reset_process(int); /*重启计时器(标号)*/
+int judge(int, int);     /*判断窗口内超时的第一个帧(窗口左,窗口右)*/
 
-struct sockaddr_in ser;
+struct sockaddr_in ser;  // 代表server
 struct Timer {
-  int left_time;
-}myTimer[TimerNum];
+	int left_time;         // 剩余时间
+}myTimer[TimerNum];      // 计时器个数上限50
 
 int main()
 {
-    s = socket(AF_INET, SOCK_STREAM, 0); /* use TCP protocol */
-  	if(s == -1){
-  		puts("Building a socket failed!");
-  		return 0;
-  	}
+	s = socket(AF_INET, SOCK_STREAM, 0); /* use TCP protocol */
+	if(s == -1){
+		puts("Building a socket failed!");
+		return 0;
+	}
 
-    ser.sin_family = AF_INET;
-    ser.sin_port = htons(6500);
-    ser.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ser.sin_family = AF_INET;
+	ser.sin_port = htons(6500);
+	ser.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(s, (struct sockaddr *) &ser, sizeof(ser));
-    puts("TCP Connection Established.");
+	connect(s, (struct sockaddr *) &ser, sizeof(ser));
+	puts("TCP Connection Established.");
 
-    puts("Enter the number of Frames: ");
-    scanf("%d", &total);
-    int2char(total);
-    send(s, act, sizeof(act), 0);
+	puts("Enter the number of Frames: ");
+	scanf("%d", &total);
+	int2char(total);
+	send(s, act, sizeof(act), 0);
 
-/* Issue: 1. No. of the frame need to begin with 0.
- *        2. Multi-thread needed
- *
-/************************* go back n **************************/
-    int i;
-    while(1) {
-        if(cur < total) {
+	/* Issue: 1. No. of the frame need to begin with 0.
+	 *        2. Multi-thread needed
+	 *
+	/************************* go back n **************************/
+	int i;
+	while(1) {
+		if(cur < total) {
 
-          for(i = 0; i < WindowSize; i++) {
-              int random = rand() % P1;
+			for(i = 0; i < WindowSize; i++) {
+				int random = rand() % P1;
 
-              // 若还能继续发就发
-              if(cur < total) {
-                // 封包
-                int2char(cur);
-                // 一定概率丢包
-                if(random < P2){
-                  send(s, bad, sizeof(bad), 0);
-                }
-                else send(s, act, sizeof(act), 0);
+				// 若还能继续发就发
+				if(cur < total) {
+					// 封包
+					int2char(cur);
+					// 一定概率丢包
+				  // printf("cur = %d, random = %d\n", cur, random);
+					if(random < P2){
+						send(s, bad, sizeof(bad), 0);
+					}
+					else send(s, act, sizeof(act), 0);
 
-                // 打开标号为cur的timer
-                timer_on(cur);
+					// 打开标号为cur的timer
+					timer_on(cur);
 
-              } // 不能发的话处理timer的问题
-              else process();
+				} // 不能发的话处理timer的问题
+				else break;
 
-              //停一秒
-              sleep(1);
+				//停一秒
+				sleep(1);
 
-              // 这里虽然显示发出,但有可能是丢包
-              if(cur < total) {
-                  printf("Frame %d Sent\n",cur);
-                  cur++;
-              }
-          }
+				// 这里虽然显示发出,但有可能是丢包
+				if(cur < total) {
+					printf("Frame %d Sent\n",cur % Mod);
+					cur++;
+				}
+			}
 
-          // 接受ACK的过程
-          int left = cur - WindowSize, right = cur;
-          int origin_left = left;
-          if(cur == total) left = total - total%WindowSize;
-          cur = left;
-          for(i = 0; i < WindowSize; i++) {
-            // 维持timer工作
-            process();
+			// 接受ACK的过程
+			int left = cur - WindowSize, right = cur;
+			int lleft = left;
+			int origin_left = left;
+			if(cur == total) left = total - total%WindowSize;
+			// cur = left;
+			// 每次最多进行WindowSize次操作
+			for(i = 0; i < WindowSize; i++) {
+				// 维持timer工作
+				process();
 
-            recv(s, act, sizeof(act), 0);
-            int now = atoi(act);
+				if(recv(s, act, sizeof(act), MSG_DONTWAIT) < 0){sleep(1);};
+				// 我想查看下是否收到东西
+				// puts(act);
+				int now = atoi(act);
 
-            //printf("left = %d right = %d\n", left, right);
+				// printf("left = %d right = %d\n", left, right);
 
-            // 判断当前窗口内的第一个超时帧,无则返回-1
-            int debug = judge(left, right);
+				// 判断当前窗口内的第一个超时帧,无则返回-1
+				int debug = judge(left, right);
 
-            printf("cur = %d debug = %d\n", cur, debug);
-            if(debug != -1 && debug > cur){
-              printf("Time-out on # %d, resent\n", debug);
-              // 待确认帧
-              cur = debug;
-              break;
-            }
-            else {
+				// printf("origin_left = %d debug = %d\n", origin_left, debug);
+				if(debug != -1 && debug > origin_left){
+					printf("Time-out on # %d, resent\n", debug % Mod);
+					// 待确认帧
+					cur = debug;
+					break;
+				}
+				else {
+					if(now >= left && now < right) {
+						// 累计应答
+						printf("The %d frame got ACK.\n", now % Mod);
 
-              //if(now >= left && now < right) {
-                // 累计应答
-                printf("The %d frame got ACK.\n", now);
+						// 窗口移动
+						int delta = now - origin_left + 1;
+						left += delta;
+						right += delta;
+						if(right > total) right = total;
+						if(left >= total) cur = total;
+						// 待确认帧
+						else origin_left = now+1;
+					}
 
-                // 窗口移动
-                int delta = now - cur + 1;
-                left += delta;
-                right += delta;
-                if(right > total) right = total;
-                if(left >= total) cur = total;
-                // 待确认帧
-                else cur = now+1;
-              //}
+				}
+				if(left - origin_left >= WindowSize) break; // 如果非正常顺序到达,已在循环结束之前将窗口跑完则直接退出
 
-            }
-            if(left - origin_left >= WindowSize) break;
-            printf("cur = %d\n", cur);
-            if(cur >= total) break;
-          }
-          if(cur >= total) break;
-          // 重置所有timer
-          reset_process(cur);
-        }
-        else {
-          //sleep();
-        }
-    }
-    close(s);
-    return 0;
+				//printf("待确认帧origin_left = %d cur = %d\n", origin_left, cur);
+				if(cur >= total) break;
+			}
+			if(origin_left < cur){ // 如果此时待确认帧还没到cur,说明在循环内没进行完确认
+				printf("Time-out on # %d, resent\n", origin_left % Mod);
+				cur = origin_left;
+			}
+			if(cur >= total) break;
+			// 重置所有timer
+			reset_process(cur);
+		}
+		else {
+			//sleep();
+		}
+	}
+	close(s);
+	return 0;
 }
 
 void int2char(int z)
@@ -155,59 +164,59 @@ void int2char(int z)
 }
 
 void timer_on(int cur){
-  setTimer(5, cur);
+	setTimer(5, cur); // 最多减6次
 
-  //等信号(异步)
-  if(signal(SIGALRM, timeout) == SIG_ERR){
-    puts("error");
-  }
+	//等信号(异步)
+	if(signal(SIGALRM, timeout) == SIG_ERR){
+		puts("error");
+	}
 
-  //发信号
-  kill(getpid(), SIGALRM);
+	//发信号
+	kill(getpid(), SIGALRM);
 
 }
 // 需要设置时长stime,标号;
 void setTimer(int stime, int cur) {
 
-  struct Timer a;
-  a.left_time = stime;
-  myTimer[cur] = a;    // 这是标号为cur的timer所对应的初始时长
-  end++;      // 这是开的timer个数的上界
+	struct Timer a;
+	a.left_time = stime;
+	myTimer[cur] = a;    // 这是标号为cur的timer所对应的初始时长
+	end++;      // 这是开的timer个数的上界
 
 }
 
 void timeout() {
 
-  int j;
-  for(j = begin; j < end; j++) { // 从第一个设置的timer开始,每次循环使各个timer减1s
-    if(myTimer[j].left_time != 1) {
-      myTimer[j].left_time--;
-    }
+	int j;
+	for(j = begin; j < end; j++) { // 从第一个设置的timer开始,每次循环使各个timer减1s
+		if(myTimer[j].left_time != 1) {
+			myTimer[j].left_time--;
+		}
 
-    printf("Timer[%d] = %d\n", j, myTimer[j].left_time);
-  }
+		//printf("Timer[%d] = %d\n", j, myTimer[j].left_time);
+	}
 
 }
 
 void process(){
-    //sleep(1);
-    kill(getpid(), SIGALRM);
-    if(signal(SIGALRM, timeout) == SIG_ERR){
-      puts("error");
-    }
+
+	if(signal(SIGALRM, timeout) == SIG_ERR){
+		puts("error");
+	}
+	kill(getpid(), SIGALRM);
 }
 
 void reset_process(int cur){
-  begin = cur;
-  end = begin;
+	begin = cur;
+	end = begin;
 }
 
 int judge(int left, int right){
-  int i;
-  for(i = left; i < right; i++){
-    if(myTimer[i].left_time == 1){
-      return i;
-    }
-  }
-  return -1;
+	int i;
+	for(i = left; i < right; i++){
+		if(myTimer[i].left_time == 1){
+			return i;
+		}
+	}
+	return -1;
 }
